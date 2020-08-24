2e7d41e89e104e19aa62002f711ebdb241d59c2e
DJ Helmick <djhelmick@gmail.com>
2020-08-23 22:29:31 -0700

    Add frontend build

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # full-stack-open-part3
+https://fs2020-part3.herokuapp.com/
---
+++ build/precache-manifest.a4d91276334f7f18804d3d2985e1ccc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b344a7ca6f0a3f9074b7274119ca2b1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c164db0d716c10a5f408",
+    "url": "/static/js/2.f7f5c33f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f7f5c33f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "981c799df993594ac47d",
+    "url": "/static/js/main.837ffe77.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
